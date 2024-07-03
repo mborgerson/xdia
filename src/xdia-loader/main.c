11cd34dd1036caccfc0b1836d458dcaf76c22961
Matt Borgerson <contact@mborgerson.com>
2024-07-03 02:26:19 -0700

    Fix xdialdr main usage

--- src/xdia-loader/main.c
+++ src/xdia-loader/main.c
@@ -6,8 +6,8 @@ wchar_t *command_line_w;
 
 int main(int argc, const char *argv[])
 {
-    assert(argc == 2 && "usage: xdia.exe <pdb>");
-    command_line_a = g_strdup_printf("xdia.exe %s", argv[1]);
+    assert(argc == 2 && "usage: xdialdr <pdb>");
+    command_line_a = g_strdup_printf("xdialdr %s", argv[1]);
     command_line_w = c2wc(command_line_a);
 
     load_pe(getenv("MSDIA_PATH") ?: "msdia140.dll");
